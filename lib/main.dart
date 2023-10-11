e2a095c76a7f89501fe8eaf05b8a113534830b00
spider <spider@darkhorse.attlocal.net>
2023-10-11 18:23:23 -0400

    first push

--- lib/main.dart
+++ lib/main.dart
@@ -27,14 +27,18 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  //setting text style
   TextStyle whiteText = TextStyle(color: Colors.white);
-  bool hideTest = false;
   Game _game = Game();
-
-  //game stats
   int tries = 0;
   int score = 0;
+
+  void resetGame() {
+    setState(() {
+      _game.initGame();
+      tries = 0;
+      score = 0;
+    });
+  }
 
   @override
   void initState() {
@@ -60,76 +64,70 @@ class _HomeScreenState extends State<HomeScreen> {
               ),
             ),
           ),
-          SizedBox(
-            height: 24.0,
-          ),
+          SizedBox(height: 24.0),
           Row(
             mainAxisAlignment: MainAxisAlignment.spaceAround,
-            crossAxisAlignment: CrossAxisAlignment.center,
             children: [
               info_card("Tries", "$tries"),
               info_card("Score", "$score"),
             ],
           ),
           SizedBox(
-              height: MediaQuery.of(context).size.width,
-              width: MediaQuery.of(context).size.width,
-              child: GridView.builder(
-                  itemCount: _game.gameImg!.length,
-                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                    crossAxisCount: 3,
-                    crossAxisSpacing: 16.0,
-                    mainAxisSpacing: 16.0,
-                  ),
-                  padding: EdgeInsets.all(16.0),
-                  itemBuilder: (context, index) {
-                    return GestureDetector(
-                      onTap: () {
-                        print(_game.matchCheck);
-                        setState(() {
-                          //incrementing the clicks
-                          tries++;
-                          _game.gameImg![index] = _game.cards_list[index];
-                          _game.matchCheck
-                              .add({index: _game.cards_list[index]});
-                          print(_game.matchCheck.first);
-                        });
-                        if (_game.matchCheck.length == 2) {
-                          if (_game.matchCheck[0].values.first ==
-                              _game.matchCheck[1].values.first) {
-                            print("true");
-                            //incrementing the score
-                            score += 100;
+            height: MediaQuery.of(context).size.width,
+            width: MediaQuery.of(context).size.width,
+            child: GridView.builder(
+              itemCount: _game.gameImg!.length,
+              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 3,
+                crossAxisSpacing: 16.0,
+                mainAxisSpacing: 16.0,
+              ),
+              padding: EdgeInsets.all(16.0),
+              itemBuilder: (context, index) {
+                return GestureDetector(
+                  onTap: () {
+                    setState(() {
+                      tries++;
+                      _game.gameImg![index] = _game.cards_list[index];
+                      _game.matchCheck.add({index: _game.cards_list[index]});
+                    });
+                    if (_game.matchCheck.length == 2) {
+                      if (_game.matchCheck[0].values.first ==
+                          _game.matchCheck[1].values.first) {
+                        score += 100;
+                        _game.matchCheck.clear();
+                      } else {
+                        Future.delayed(Duration(milliseconds: 500), () {
+                          setState(() {
+                            _game.gameImg![_game.matchCheck[0].keys.first] =
+                                _game.hiddenCardpath;
+                            _game.gameImg![_game.matchCheck[1].keys.first] =
+                                _game.hiddenCardpath;
                             _game.matchCheck.clear();
-                          } else {
-                            print("false");
-
-                            Future.delayed(Duration(milliseconds: 500), () {
-                              print(_game.gameColors);
-                              setState(() {
-                                _game.gameImg![_game.matchCheck[0].keys.first] =
-                                    _game.hiddenCardpath;
-                                _game.gameImg![_game.matchCheck[1].keys.first] =
-                                    _game.hiddenCardpath;
-                                _game.matchCheck.clear();
-                              });
-                            });
-                          }
-                        }
-                      },
-                      child: Container(
-                        padding: EdgeInsets.all(16.0),
-                        decoration: BoxDecoration(
-                          color: Color(0xFFFFB46A),
-                          borderRadius: BorderRadius.circular(8.0),
-                          image: DecorationImage(
-                            image: AssetImage(_game.gameImg![index]),
-                            fit: BoxFit.cover,
-                          ),
-                        ),
+                          });
+                        });
+                      }
+                    }
+                  },
+                  child: Container(
+                    padding: EdgeInsets.all(16.0),
+                    decoration: BoxDecoration(
+                      color: Color(0xFFFFB46A),
+                      borderRadius: BorderRadius.circular(8.0),
+                      image: DecorationImage(
+                        image: AssetImage(_game.gameImg![index]),
+                        fit: BoxFit.cover,
                       ),
-                    );
-                  }))
+                    ),
+                  ),
+                );
+              },
+            ),
+          ),
+          ElevatedButton(
+            onPressed: resetGame,
+            child: Text('Reset Game'),
+          ),
         ],
       ),
     );
